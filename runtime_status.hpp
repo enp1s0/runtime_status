7ba35a5d25b9ddf08eb245574fe5d67fb4e12fbb
mutsuki <mutsuki@momo86.net>
2020-04-25 11:35:31 +0900

    Create runtime_status.hpp

---
+++ runtime_status.hpp
@@ -0,0 +1,13 @@
+#ifndef __RUNTIME_STATUS_HPP__
+#define __RUNTIME_STATUS_HPP__
+#include <cstdlib>
+
+namespace mtk {
+namespace runtime_status {
+inline void print_cpu_info() {
+	
+}
+} // namespace runtime_status
+} // namespace mtk
+
+#endif /* end of include guard */
