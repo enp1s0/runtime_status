39f0d71f145bdb648f271b38b63aa8ba8dbb53e1
mutsuki <mutsuki@momo86.net>
2020-04-25 12:51:24 +0900

    Add `RS_GIT_COMMIT`

--- runtime_status.hpp
+++ runtime_status.hpp
@@ -1,11 +1,20 @@
 #ifndef __RUNTIME_STATUS_HPP__
 #define __RUNTIME_STATUS_HPP__
+#include <iostream>
 #include <cstdlib>
+
+#ifndef RS_GIT_BRANCH
+#define RS_GIT_BRANCH "NOT PROVIDED"
+#endif
+#ifndef RS_GIT_COMMIT
+#define RS_GIT_COMMIT "NOT PROVIDED"
+#endif
 
 namespace mtk {
 namespace runtime_status {
-inline void print_cpu_info() {
-	
+inline void print_git_info() {
+	std::printf("%25s : %s\n", "commit at building", RS_GIT_COMMIT);
+	std::printf("%25s : %s\n", "branch at building", RS_GIT_BRANCH);
 }
 } // namespace runtime_status
 } // namespace mtk
