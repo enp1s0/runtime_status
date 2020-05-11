c1903f64004d5ce3eea0b33391ce8790fa987af3
mutsuki <mutsuki@momo86.net>
2020-05-11 23:48:18 +0900

    Add `print_process_using_memory`

--- process_status.hpp
+++ process_status.hpp
@@ -13,6 +13,13 @@ void print_process_info() {
 	std::printf("%10s : %s\n", "Name", proc_status.get_Name().c_str());
 	std::printf("%10s : %s\n", "State", proc_status.get_State().c_str());
 }
+
+void print_process_using_memory() {
+	proc_status::proc_status proc_status;
+	proc_status.load_self_info();
+
+	std::printf("# process information\n");
+	std::printf("%10s : %u\n", "Vmem", proc_status.get_VmSize());
 }
 } // namespace runtime_status
 } // namespace mtk
