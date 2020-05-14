0f51ac02d9ab8a922da8b9b8f67ef18de4acb4b8
mutsuki <mutsuki@momo86.net>
2020-05-14 23:43:38 +0900

    Add `print_header`

--- test/process_status.cpp
+++ test/process_status.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
 #include <process_status.hpp>
 
+constexpr bool print_header = true;
+
 int main() {
-	mtk::runtime_status::process::print_info();
-	mtk::runtime_status::process::print_using_memory_size();
+	mtk::runtime_status::process::print_info(print_header);
+	mtk::runtime_status::process::print_using_memory_size(print_header);
 }
