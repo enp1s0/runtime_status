17199c101ceb99bc7d028bf2221438c12b7a6bab
mutsuki <mutsuki@momo86.net>
2020-05-14 23:34:14 +0900

    Add `print_header` switch in `print_using_memory_size` and `print_info`

--- process_status.hpp
+++ process_status.hpp
@@ -6,20 +6,24 @@
 namespace mtk {
 namespace runtime_status {
 namespace process {
-void print_info() {
+void print_info(const bool print_header = false) {
 	proc_status::proc_status proc_status;
 	proc_status.load_self_info();
 
-	std::printf("# process information\n");
+	if (print_header) {
+		std::printf("# process information\n");
+	}
 	std::printf("%10s : %s\n", "Name", proc_status.get_Name().c_str());
 	std::printf("%10s : %s\n", "State", proc_status.get_State().c_str());
 }
 
-void print_using_memory_size() {
+void print_using_memory_size(const bool print_header = false) {
 	proc_status::proc_status proc_status;
 	proc_status.load_self_info();
 
-	std::printf("# process information\n");
+	if (print_header) {
+		std::printf("# process information\n");
+	}
 	std::printf("%10s : %u\n", "Vmem", proc_status.get_VmSize());
 }
 } // namespace process
