1a75848f53f055bc9344adc1792bab5d6eff4331
mutsuki <mutsuki@momo86.net>
2020-09-04 23:09:36 +0900

    Add env.hpp

---
+++ runtime_staus/env.hpp
@@ -0,0 +1,11 @@
+#ifndef __RUNTIME_STATUS_ENV_HPP__
+#define __RUNTIME_STATUS_ENV_HPP__
+
+namespace mtk {
+namespace runtime_status {
+namespace env {
+
+} // namespace env
+} // namespace runtime_status
+} // namespace mtk
+#endif
