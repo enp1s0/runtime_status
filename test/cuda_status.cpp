bf9637eb5b4cab44f30b92856dbb9ba7f0f153c0
mutsuki <mutsuki@momo86.net>
2020-10-18 13:41:53 +0900

    Update cuda_status for printing NVCC version

--- test/cuda_status.cpp
+++ test/cuda_status.cpp
@@ -6,5 +6,6 @@ constexpr bool print_header = true;
 int main() {
 	mtk::runtime_status::cuda::print_info(print_header);
 	mtk::runtime_status::cuda::print_current_device_id(print_header);
-	std::cout << __CUDACC_VER_MAJOR__ << std::endl;
+
+	std::cout << __CUDACC_VER_BUILD__ << std::endl;
 }
