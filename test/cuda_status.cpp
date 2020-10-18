dcc96114313ff741414480d1bd97c97ec92a78ce
mutsuki <mutsuki@momo86.net>
2020-10-18 13:42:23 +0900

    Remove a debug message

--- test/cuda_status.cpp
+++ test/cuda_status.cpp
@@ -6,6 +6,4 @@ constexpr bool print_header = true;
 int main() {
 	mtk::runtime_status::cuda::print_info(print_header);
 	mtk::runtime_status::cuda::print_current_device_id(print_header);
-
-	std::cout << __CUDACC_VER_BUILD__ << std::endl;
 }
